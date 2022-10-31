b4436f1fe62acaf2db20203d45bd2582471c605e
unknown <santiposse2000@gmail.com>
2022-10-31 19:37:53 -0300

    firt commit

---
+++ src/resumeData.js
@@ -0,0 +1,92 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Rahul Bhatia",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/santiago-jos%C3%A9-posse-ar%C3%A1oz/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/SantiagoPosseAraoz",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Argentina",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"Plataforma 5 - Coding bootcamp",
+        "specialization":"Full-Stack developer",
+        "MonthOfPassing":"Oct",
+        "YearOfPassing":"2022",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Globant",
+        "specialization":"Full-Stack developer",
+        "MonthOfLeaving":"Oct",
+        "YearOfLeaving":"2022",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"V-Book - E-commerce",
+        "specialization":"Full-Stack developer",
+        "MonthOfLeaving":"Sept",
+        "YearOfLeaving":"2022",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Node.js"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Typescript"
+      },
+      {
+        "skillname":"Reactjs"
+      }      
+    ],
+    "portfolio":[
+      {
+        "name":"V-Book",
+        "description":"E-Commerce",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"TMDB",
+        "description":"Class project",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
