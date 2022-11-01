7bdbebbf6d242b176121d3381e33ae68ee0bde83
unknown <santiposse2000@gmail.com>
2022-11-01 17:32:11 -0300

    linkedIn contact

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -8,7 +8,12 @@ export default class ContactUs extends Component {
           <div className="ten columns">
             <a href="mailto:santiposse2000@gmail.com?subject = Contact&body = Message">
               <p className="lead">
-                Contact me
+                Send me an e-mail
+              </p>
+            </a>
+            <a href="https://www.linkedin.com/in/santiago-jos%C3%A9-posse-ar%C3%A1oz/" target="_blank" rel="noopener noreferrer">
+              <p className="lead">
+                Add me on LinkedIn
               </p>
             </a>
           </div>
